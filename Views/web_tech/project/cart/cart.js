584d17ca5c92258c5b36bdd9397084db5573c6dd
lecuong35 <92246385+lecuong35@users.noreply.github.com>
2022-05-30 21:11:39 +0700

    Add files via upload

---
+++ Views/web_tech/project/cart/cart.js
@@ -0,0 +1,7 @@
+function order() {
+    document.getElementById('bill-form').style.visibility = "visible";
+}
+
+function orderPay() {
+    document.getElementById('bill-form').style.visibility = "hidden";
+}
